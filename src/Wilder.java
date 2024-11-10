a630695e56c33751d1ebef4d8f204763cc9bd424
alex <alexandre.ag.67@gmail.com>
2024-11-10 14:39:37 +0100

    Ajout du constructeur pour initialiser firstname et aware.

--- src/Wilder.java
+++ src/Wilder.java
@@ -3,4 +3,10 @@ public class Wilder {
     private String firstname;
     private boolean aware;
 
+    public Wilder(String firstname, boolean aware) {
+        this.firstname = firstname;
+        this.aware = aware;
+    }
+
+
 }
